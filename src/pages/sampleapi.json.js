e773ccc6b3b9fe2cc12bdab3f28f5623e1db473c
Arun visvajeet RS <arun.visvajeet@posten.no>
2023-05-28 12:45:22 +0200

    del sampleapi file

--- src/pages/sampleapi.json.js
+++
@@ -1,9 +0,0 @@
-export async function get({params, request}) {
-    return {
-      body: JSON.stringify({
-        name: 'Try Astro',
-        body: 'Astro demo in the url',
-        url: 'https://localhost:3000/',
-      }),
-    };
-  }
