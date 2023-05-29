ff4ff28551ca06de8d373281986086caa32284ef
Arun visvajeet RS <arun.visvajeet@posten.no>
2023-05-29 14:41:03 +0200

    Add tina

---
+++ tina/config.ts
@@ -0,0 +1,69 @@
+import { defineConfig } from "tinacms";
+
+// Your hosting provider likely exposes this as an environment variable
+const branch = process.env.HEAD || process.env.VERCEL_GIT_COMMIT_REF || "main";
+
+export default defineConfig({
+  branch,
+  clientId: "1de93cf8-dc4f-4085-9840-368535d3bb1b", // Get this from tina.io
+  token: "3fe2438b1ffce6eecbb1c5ef4e75cafd203dbd64", // Get this from tina.io
+
+  build: {
+    outputFolder: "admin",
+    publicFolder: "public",
+  },
+  media: {
+    tina: {
+      mediaRoot: "",
+      publicFolder: "public",
+    },
+  },
+  schema: {
+    collections: [
+      {
+        name: "post",
+        label: "Posts",
+        path: "src/posts",
+        fields: [
+          {
+            type: "string",
+            name: "title",
+            label: "Title",
+            isTitle: true,
+            required: true,
+          },
+          {
+            type: "string",
+            name: "description",
+            label: "Description",
+            required: true,
+          },
+          {
+            type: "string",
+            name: "bannerImage",
+            label: "Banner Image",
+            required: true,
+          },
+          {
+            type: "string",
+            name: "slug",
+            label: "Path",
+            required: true,
+          },
+          {
+            type: "string",
+            name: "category",
+            label: "Category",
+            required: true,
+          },
+          {
+            type: "rich-text",
+            name: "body",
+            label: "Body",
+            isBody: true,
+          },
+        ],
+      },
+    ],
+  },
+});
